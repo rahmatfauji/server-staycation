0165c4f5ae1a5f0d0da6cbb5e8b2cdc59d84d4d5
Rahmat Fauji <rahmat.fauji@emveep.com>
2021-09-06 10:10:20 +0700

    membuat menu dashboard

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var adminRouter = require('./routes/admin');
 var app = express();
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use('/sb-admin-2',express.static(path.join(__dirname, 'node_modules/startboo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin',adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
